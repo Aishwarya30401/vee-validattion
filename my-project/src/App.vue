3fea51866df713d7d8ca7726afdada18909f2a59
Aishwarya30401 <aishu30401@gmail.com>
2022-11-02 15:18:25 -0700

    veelaidation

---
+++ my-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- <BuefyForm /> -->
+    <RefactoredForm />
+  </div>
+</template>
+
+<script>
+import Form from "./components/Form";
+import RefactoredForm from "./components/RefactoredForm";
+
+export default {
+  name: "App",
+  components: {
+    BuefyForm: Form,
+    RefactoredForm
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
